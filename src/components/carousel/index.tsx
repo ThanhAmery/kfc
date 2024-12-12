ee7cb1ad9ea07d79f6d60736105125db4a3ea7c2
Công Thành <dct01112004@gmail.com>
2024-12-12 20:33:15 +0700

    update carousel for home page

---
+++ src/components/carousel/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+
+
+import "./index.scss"; 
+import { Autoplay, Navigation, } from "swiper/modules";
+
+const Carousel: React.FC = () => { // React.FC is a generic type that is used to define the type of props that a component can accept.
+  const images = [
+    "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/DoubleDay.webp?v=3J89N4",
+    "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/DealTA.webp?v=3J89N4",
+  ];
+
+  return (
+    <div>
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        // pagination={{ clickable: true }}
+        navigation 
+        autoplay={{ delay: 3000, disableOnInteraction: false, }}  // disableOnInteraction: true means that the autoplay will be stopped when the user interacts with the slider.
+        
+        className="carousel"
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img src={image} alt={`Slide ${index + 1}`} style={{ width: "100%", height: "auto" }} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Carousel;
+
+
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,8 +1,10 @@
+import Carousel from "../../components/carousel"
 import FoodList from "../../components/food-list"
 
 function Home() {
   return (
     <div>
+      <Carousel />
       <FoodList />
       <h1>Home</h1>
     </div>
