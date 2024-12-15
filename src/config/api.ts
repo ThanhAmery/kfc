de6d4cc47d2d5ac78b2e2ef3351d0f3e3177d3f2
Công Thành <dct01112004@gmail.com>
2024-12-15 21:25:29 +0700

    update config api

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     //baseURL: 'http://152.42.216.35:8080/api/', // server BE 
-    baseURL: 'http://142.93.49.109:8080/api/', // server BE
+    // baseURL: 'http://142.93.49.109:8080/api/', // server BE
+    baseURL: 'https://674c709c54e1fca9290c90bc.mockapi.io/', // server BE
   });
 
 export default api;
