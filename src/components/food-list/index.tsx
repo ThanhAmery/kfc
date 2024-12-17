28c4de2b2e08b7363b53f0b9686f75c883360f21
Công Thành <dct01112004@gmail.com>
2024-12-17 19:57:34 +0700

    add carousel to foood-listlist

--- src/components/food-list/index.tsx
+++ src/components/food-list/index.tsx
@@ -4,6 +4,8 @@ import api from "../../config/api";
 import { toast } from "react-toastify";
 import Card from "../card";
 import "./index.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 
 function FoodList() {
 
@@ -25,9 +27,24 @@ function FoodList() {
 
   return (
     <div className="food-list">
-        {foods.map((food) => (
+        {/* {foods.map((food) => (
             <Card food={food} key={food.id} />
-        ))}
+        ))} */}
+
+        <Swiper
+            modules={[Navigation, Pagination]} 
+            spaceBetween={30} 
+            slidesPerView={4} 
+            navigation 
+            pagination={{ clickable: true }} 
+            loop={true} 
+        >
+            {foods.map((food) => (
+            <SwiperSlide key={food.id}>
+                <Card food={food} />
+            </SwiperSlide>
+            ))}
+        </Swiper>
 
     </div>
   )
