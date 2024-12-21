75ed562dda828c223b823dc3f84345e11f40bb7b
Công Thành <dct01112004@gmail.com>
2024-12-21 11:49:05 +0700

    add header to cart page

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,52 +7,117 @@ function Header() {
 
     const location = useLocation(); // lấy ra thông tin của url hiện tại 
 
-   return (
-    <div className="header">
-        <div className="header__left">
-            <img 
-            src="https://static.kfcvietnam.com.vn/images/web/kfc-logo.svg?v=5.0" 
-            alt="logo" 
-            className="header__logo" 
-            width={50}
-            onClick={() => navigate("/")}
-            />
-            <ul className="header__navigation">
-                <li>Thực đơn</li>
-                <li>Khuyến mãi</li>
-                <li>Dịch vụ</li>
-                <li>Hệ thống nhà hàng</li>
+    const isCartPage = location.pathname === "/cart";
 
-            </ul>
-        </div>
-        <div className="header__right">
+    return (
+        <div className={`header ${isCartPage ? "header--cart" : ""}`}>
+          <div className="header__left">
+            {isCartPage ? (
+              <button
+                className="header__back"
+                onClick={() => navigate("/")}
+              >
+                Quay lại menu
+              </button>
+              
+            ) : (
+              // Logo cho các trang khác
+              <img
+                src="https://static.kfcvietnam.com.vn/images/web/kfc-logo.svg?v=5.0"
+                alt="logo"
+                className="header__logo"
+                width={50}
+                onClick={() => navigate("/")}
+              />
+            )}
 
-            <div className="header__account">
-            <UserOutlined 
-            size={100} 
-            className="icon" 
-            onClick={() => navigate("/login")}/>
+            {/* Navigation Links */}
+            {!isCartPage && (
+                <ul className="header__navigation">
+                    <li>Thực đơn</li>
+                    <li>Khuyến mãi</li>
+                    <li>Dịch vụ</li>
+                    <li>Hệ thống nhà hàng</li>
+                </ul>
+            )}
+          </div>
+    
+          {isCartPage && (
+              <img width={100} alt="logo"
+              src="https://static.kfcvietnam.com.vn/images/web/kfc-logo-mobile.svg?v=5.0"
+              />
+          )}
+          
+          
+            <div className="header__right">
+              <div className="header__account">
+                <UserOutlined
+                  size={100}
+                  className="icon"
+                  onClick={() => navigate("/login")}
+                />
+              </div>
+
+              {!isCartPage && (
+              <div className="header__cart" onClick={() => navigate("/cart")}>
+                <span className="number">0</span>
+              </div>
+              )}
+    
+              <div className="header__menu">
+                <MenuOutlined />
+              </div>
             </div>
+          
+        </div>
+      );
+
+//    return (
+//     <div className="header">
+//         <div className="header__left">
+//             <img 
+//             src="https://static.kfcvietnam.com.vn/images/web/kfc-logo.svg?v=5.0" 
+//             alt="logo" 
+//             className="header__logo" 
+//             width={50}
+//             onClick={() => navigate("/")}
+//             />
+//             <ul className="header__navigation">
+//                 <li>Thực đơn</li>
+//                 <li>Khuyến mãi</li>
+//                 <li>Dịch vụ</li>
+//                 <li>Hệ thống nhà hàng</li>
+
+//             </ul>
+//         </div>
+//         <div className="header__right">
+
+//             <div className="header__account">
+//             <UserOutlined 
+//             size={100} 
+//             className="icon" 
+//             onClick={() => navigate("/login")}/>
+//             </div>
 
         
-            <div className="header__cart" onClick={() => navigate("/cart")}>
-                {/* <img 
-                src="https://i.ibb.co/C7xB7MW/Screenshot-2024-07-08-at-19-22-32-removebg-preview.png"
-                alt="cart"
-                className="cart__icon"
-                width={50}
-                /> */}
-                <span className="number">0</span>
-            </div>
+//             <div className="header__cart" onClick={() => navigate("/cart")}>
+//                 {/* <img 
+//                 src="https://i.ibb.co/C7xB7MW/Screenshot-2024-07-08-at-19-22-32-removebg-preview.png"
+//                 alt="cart"
+//                 className="cart__icon"
+//                 width={50}
+//                 /> */}
+//                 <span className="number">0</span>
+//             </div>
 
-            <div className="header__menu">
-            <MenuOutlined />
-            </div>
+//             <div className="header__menu">
+//             <MenuOutlined />
+//             </div>
 
             
-        </div>
-    </div>
-   )
+//         </div>
+//     </div>
+//    )
 }
 
 export default Header
