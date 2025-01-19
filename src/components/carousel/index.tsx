d2fdc09db2ca27f86f828aa9d30e3b1e1f6ff953
Công Thành <dct01112004@gmail.com>
2025-01-19 20:54:39 +0700

    add comment for images to carousel

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -9,13 +9,13 @@ import { Autoplay, Navigation, } from "swiper/modules";
 
 const Carousel: React.FC = () => { // React.FC is a generic type that is used to define the type of props that a component can accept.
   const images = [
-    // "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/DoubleDay.webp?v=3J89N4",
-    "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/Chizza.webp?v=46KM9g",
-    "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/ComboTet.webp?v=3QjJEL",
-    "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/Voucher.webp?v=4BoXag",
-    // "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/ComboSting.webp?v=gbm184",
+    // "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/DoubleDay.webp?v=3J89N4",    //Christmas
+    "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/Chizza.webp?v=46KM9g",          //Chizza
+    "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/ComboTet.webp?v=3QjJEL",        //combo Tết
+    "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/Voucher.webp?v=4BoXag",         //Lì xì bạn mới
+    //  "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/ComboSting.webp?v=gbm184",  //combo có sting 
     "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/Festive.webp?v=gbm184",
-    "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/Dinner-Dec.webp?v=gbm184",
+    // "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/Dinner-Dec.webp?v=gbm184",   //combo gia đình 
     "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/TNAGP4.webp?v=K4P1bg",
     "https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/DealTA.webp?v=3J89N4",
   ];
